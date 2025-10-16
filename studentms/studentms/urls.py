cc05b7aed983ca5a007c01ad14aae9b0f95a546b
ivyadisa <ivyadisa7@gmail.com>
2025-10-16 21:35:36 +0300

    Fix allowed hosts for render

--- studentms/studentms/urls.py
+++ studentms/studentms/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/', include('student.urls')),\
+    path('', include('student.urls')),\
     # path("accounts/", include("django.contrib.auth.urls"))
 ]
